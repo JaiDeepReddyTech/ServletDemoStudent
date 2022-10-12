a5655ace6b4f4f33cc99bcf0f7975a66d33d16b7
Jaideep Reddy <jaideepreddy197@gmail.com>
2022-10-12 13:12:36 +0530

    change

--- src/com/mvc/demo/MVCDemoServlets.java
+++ src/com/mvc/demo/MVCDemoServlets.java
@@ -15,7 +15,7 @@ public class MVCDemoServlets extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     public MVCDemoServlets() {
         super();
-        // TODO Auto-generated constructor stub
+       
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
